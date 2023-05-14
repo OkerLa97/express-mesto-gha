f57607cfd1967c0ff9d34c9ce4e1e5bc1b799aec
Oker <oker97@mail.ru>
2023-05-14 15:58:30 +0200

    Правка роутов

--- routes/signin.js
+++ routes/signin.js
@@ -2,7 +2,7 @@ const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
 const { login } = require('../controllers/users');
 
-router.post('/signin', celebrate({
+router.post('/', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8).max(30),
--- routes/signup.js
+++ routes/signup.js
@@ -2,7 +2,7 @@ const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
 const { createUser } = require('../controllers/users');
 
-router.post('/signup', celebrate({
+router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
