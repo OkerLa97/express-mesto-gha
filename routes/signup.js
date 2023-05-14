f9e295d172b81b455624a4f9b43fd653145553d4
Oker <oker97@mail.ru>
2023-05-14 16:02:57 +0200

    Правка проверки обязательных полей регистрации

--- routes/signup.js
+++ routes/signup.js
@@ -4,13 +4,13 @@ const { createUser } = require('../controllers/users');
 
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
     avatar: Joi.string()
       .required()
       .regex(/^(https?:\/\/)([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?#?$/),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8).max(30),
   }),
 }), createUser);
 
