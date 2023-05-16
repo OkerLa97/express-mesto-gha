22def67f0acc2eb78f9c5266e1f15405599222fa
Oker <oker97@mail.ru>
2023-05-16 17:32:59 +0200

    вынес функцию для централизованной обработки ошибок в отдельный файл

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000, BASE_PATH } = process.env;
 const app = express();
@@ -37,18 +38,7 @@ app.use((req, res) => {
 app.use(errors());
 
 // здесь обрабатываем все ошибки
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-
-  res.status(statusCode).send({
-    // проверяем статус и выставляем сообщение в зависимости от него
-    message: statusCode === http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR
-      ? 'На сервере произошла ошибка'
-      : message,
-  });
-
-  next();
-});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log('Ссылка на сервер');
